42f0137633f49a04c1b013e8f26871d5160eda12
ssheikholeslami <ssheikholeslami@gmail.com>
2025-04-10 15:55:51 +0000

    Deploying to gh-pages from @ ssheikholeslami/ssheikholeslami.github.io@4f13dff70c947554fbb12b6e8c6fbdca625dfe22 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,18 +433,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/samples/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-i-successfully-defended-my-phd-thesis",
+            },},{id: "news-i-successfully-defended-my-phd-thesis",
           title: 'I successfully defended my PhD thesis',
           description: "",
           section: "News",},{id: "projects-project-1",
